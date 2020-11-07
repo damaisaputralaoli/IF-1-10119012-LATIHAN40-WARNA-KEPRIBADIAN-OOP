6e3113fd1000b36d7855e3f782d7381177e1f2d0
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-08 01:16:05 +0700

    Add files via upload

---
+++ PBO10119012Latihan40.java
@@ -0,0 +1,20 @@
+
+package pbo10119012latihan40;
+
+/**
+ *
+ * @author damai
+ * NAMA  : DAMAI SAPUTRA LAOLI
+ * KELAS : IF-1
+ * NIM   : 10119012
+ */
+public class PBO10119012Latihan40 {
+
+    
+    public static void main(String[] args) {
+       Kepribadian orang = new Kepribadian();
+        orang.getNama();
+        orang.getWarna();
+        orang.getKepribadian();
+    }
+}
